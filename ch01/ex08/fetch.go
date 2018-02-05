c640003ffc2c136b87a1e106b0de93820d554629
takahashikenichi <takahashi.kenichi@gmail.com>
2018-02-05 11:50:36 +0900

    Added ex08 files

---
+++ ch01/ex08/fetch.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"io"
+//	"io/ioutil"
+	"net/http"
+	"os"
+	"strings"
+)
+
+const HTTP_PROTOCOL_INDICATER = "http://"
+
+func main() {
+	for _, url := range os.Args[1:] {
+		if !(strings.HasPrefix(url, HTTP_PROTOCOL_INDICATER)) {
+			url = HTTP_PROTOCOL_INDICATER + url 	
+		}
+
+		resp, err := http.Get(url)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
+			os.Exit(1)
+		}
+		
+		nbytes, err := io.Copy(os.Stdout, resp.Body)	
+		resp.Body.Close()
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: reading %s: %v\n", url, err)
+			os.Exit(1)
+		}
+		fmt.Printf("\n%v bite(s) fetched. \n", nbytes)
+	}
+}
---
+++ ch01/ex08/run.bash
@@ -0,0 +1,3 @@
+go build fetch.go
+./fetch http://gopl.io
+./fetch gopl.io
