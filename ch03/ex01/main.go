a0fcb29ab7639a5cf17714f18d2669c3f9af743c
髙橋　憲一 <01012629@CA3188.local>
2018-03-16 04:36:52 +0900

    fix bug main.go

--- ch03/ex01/main.go
+++ ch03/ex01/main.go
@@ -42,7 +42,7 @@ func corner(i, j int) (float64, float64) {
 	// Compute surface height z.
 	z := f(x, y)
 
-	if math.IsNaN(z) || math.IsInf(z) {
+	if math.IsNaN(z) || math.IsInf(z, 0) {
 		return 0, 0, errors.New("Invalid value :z")
 	}
 
