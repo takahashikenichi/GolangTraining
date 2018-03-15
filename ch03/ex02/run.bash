bbd960bca82c09e9482e8e1e3b01bbdd74b44fc9
髙橋　憲一 <01012629@CA3188.local>
2018-03-16 06:18:39 +0900

    add ex01,ex02

---
+++ ch03/ex02/run.bash
@@ -0,0 +1,8 @@
+go run main.go > image/polygon.svg
+sudo open -a Safari.app image/polygon.svg 
+go run box_of_egg.go > image/box_of_egg.svg
+sudo open -a Safari.app image/box_of_egg.svg 
+go run mogul.go > image/mogul.svg
+sudo open -a Safari.app image/mogul.svg 
+go run saddle.go > image/saddle.svg
+sudo open -a Safari.app image/saddle.svg 
