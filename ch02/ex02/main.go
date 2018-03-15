577fbbf2fd86e31dc9f4e10e2406d6aaf1ba4df5
髙橋　憲一 <01012629@CA3188.local>
2018-03-15 22:36:38 +0900

    update ch02/ex02

---
+++ ch02/ex02/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+//	"./src"
+	"github.com/takahashikenichi/GolangTraining/ch02/ex02/tempconv"
+	"github.com/takahashikenichi/GolangTraining/ch02/ex02/distconv"
+//	"github.com/takahashikenichi/GolangTraining/ch02/ex02/weightconv"
+)
+
+func main() {
+	if len(os.Args[1:]) < 2 {
+		fmt.Println("input: Error argument.");
+		os.Exit(1)
+	}
+	args1 := os.Args[1]
+	args2 := os.Args[2]
+
+	value, err := strconv.ParseFloat(args2, 64)
+
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Args[2] is not collect: %value\n")
+		os.Exit(1)
+	}
+
+	switch args1 {
+		case "t":
+			c := tempconv.Celsius(value)
+			f := tempconv.Fahrenheit(value)
+			fmt.Printf("%s = %s, %s = %s\n", f, tempconv.FToC(f), c, tempconv.CToF(c))
+		case "d":
+			m := distconv.Meter(value)
+			f := distconv.Feet(value)
+			fmt.Printf("%s = %s, %s = %s\n", m, distconv.MToF(m), f, distconv.FToM(f))
+		case "w":
+			p := weightconv.Pound(value)
+			k := weightconv.KiloGram(value)
+			fmt.Printf("%s = %s, %s = %s\n", p, weightconv.PToK(p), k, weightconv.PToK(p))
+	}
+}
---
+++ ch02/ex02/run.bash
@@ -0,0 +1,9 @@
+go get github.com/takahashikenichi/GolangTraining/ch02/ex02/tempconv
+go get github.com/takahashikenichi/GolangTraining/ch02/ex02/distconv
+go get github.com/takahashikenichi/GolangTraining/ch02/ex02/weightconv
+
+go build main.go
+
+./go t 200
+./go d 200
+./go w 200
